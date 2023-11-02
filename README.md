cf30c64ffc6d424d62dc0f9cb58b9e90b08719b0
Krzysztof Wiatrzyk <krzysztof.wiatrzyk@airhelp.com>
2023-11-02 20:10:38 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gtask
 Global Taskfile with a lot of commands
+
+## Install
+
+```
+wget -O - https://raw.githubusercontent.com/krzwiatrzyk/gtask/main/webi.sh | bash 
+```
---
+++ webi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+# Install Taskfile
+sudo sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b /usr/local/bin
+
+# Clone repository for machine management if it doesn't exist
+test ! -d "${HOME}/.task" && git clone https://github.com/krzwiatrzyk/gtask.git ${HOME}/.task
+cp ${HOME}/.task/Taskfile.yaml ${HOME}/Taskfile.yaml
+
