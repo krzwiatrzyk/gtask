eca278dac99b5d8ab13e58fab82f7fc28d8c201a
Krzysztof Wiatrzyk <krzwiatrzyk@protonmail.com>
2024-04-10 20:04:26 +0200

    feat: use remote taskfiles

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ wget -O - https://raw.githubusercontent.com/krzwiatrzyk/gtask/main/webi.sh | bas
 
 - `task -g -a` - list all tasks
 - `task -g <task>` - execute task
+
+> [!IMPORTANT]  
+> Global taskfile uses **remote taskfiles** functionality
+> that requires environment variable `TASK_X_REMOTE_TASKFILES=1` to be set to make it work properly.
+> Read more: https://taskfile.dev/experiments/remote-taskfiles/
+
--- webi.sh
+++ webi.sh
@@ -7,7 +7,11 @@ if ! command -v task >> /dev/null; then
   sudo sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b /usr/local/bin
 fi
 
-# Clone repository if it doesn't exist
-test ! -d "${HOME}/.task" && git clone https://github.com/krzwiatrzyk/gtask.git ${HOME}/.task
-cp ${HOME}/.task/TaskfileGlobal.yaml ${HOME}/Taskfile.yaml
+# Download Global Taskfile
+wget https://raw.githubusercontent.com/krzwiatrzyk/gtask/main/TaskfileGlobal.yaml -O ${HOME}/Taskfile.yaml
+
+
+# Download Global Taskfile repo
+# test ! -d "${HOME}/.task" && git clone https://github.com/krzwiatrzyk/gtask.git ${HOME}/.task
+# cp ${HOME}/.task/TaskfileGlobal.yaml ${HOME}/Taskfile.yaml
 
